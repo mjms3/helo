e59023f856ef05411dc6ee528bef672f18ec2237
mjms3 <shepherd.max@gmail.com>
2017-01-07 13:55:19 +0000

    Triangulation example

---
+++ sandbox/triangulation_display.py
@@ -0,0 +1,25 @@
+import matplotlib
+
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+import numpy as np
+
+
+
+vertices = np.asarray([[0, 0],
+                       [1, 0],
+                       [0, 1],
+                       [1, 1],
+                       [1 / 2, 1 / 2]], dtype=np.float64)
+
+edges = np.asarray([[0, 2, 4],
+                   [0, 4, 1],
+                   [1, 4, 3],
+                   [4, 2, 3]])
+
+triangulation = tri.Triangulation(vertices[:,0],vertices[:,1], triangles=edges)
+
+plt.figure()
+plt.gca().set_aspect('equal')
+plt.triplot(triangulation, 'bo-')
+plt.show()
