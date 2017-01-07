ca7af519ff743dbe63aed89b8b1e713e17dfd8fe
mjms3 <shepherd.max@gmail.com>
2017-01-07 17:38:53 +0000

    Refactor shortest path logic into class.
    Also add logic to ensure correct triangle is returned when on a boundary.

--- esp/esp.py
+++ esp/esp.py
@@ -1,11 +1,93 @@
-import numpy as np
+from math import sqrt
 
-def triangulation_to_graph(triangulation):
-    graph = []
-    for edge in triangulation.edges:
-        v1,v2 = edge
-        xdiff = triangulation.x[v1]-triangulation.x[v2]
-        ydiff = triangulation.y[v1]-triangulation.y[v2]
-        norm = np.sqrt(xdiff**2+ydiff**2)
-        graph.append((edge[0], edge[1], norm))
-    return graph
+import numpy as np
+import matplotlib.tri as tri
+
+from esp.dijkstra import dijkstra
+
+
+class ShortestPathFinder(object):
+    def __init__(self, vertices, triangles, triangle_weights=None):
+        self.vertices = vertices
+        self.triangles = triangles
+        self.triangle_weights = triangle_weights or [1 for _ in triangles]
+        self.triangulation = tri.Triangulation([v[0] for v in vertices],
+                                               [v[1] for v in vertices],
+                                               triangles=triangles)
+
+        self.tri_finder = self.triangulation.get_trifinder()
+
+    def get_path_cost(self, start, end):
+        """
+        Get cost of path.
+
+        Start and end points are assumed (by construction) to be on edge
+        or vertices of a triangle.
+
+        :param start:
+        :param end:
+        :return:
+        """
+        s, e = start, end
+        dx = e[0] - s[0]
+        dy = e[1] - s[1]
+        m = dy/dx
+
+        x0 = s[0] + 1 / 2 * (dx)
+        y0 = s[1] + 1 / 2 * (dy)
+        path_mid_point = (x0,
+                          y0)
+
+        p = path_mid_point
+
+        y_perp = lambda x: -1/m*x+(y0-x0/m)
+
+        eps = 1e-8
+        point1 = (p[0] + eps, p[1] + eps * y_perp(eps))
+        point2 = (p[0] - eps, p[1] - eps * y_perp(eps))
+
+        triangle1 = self.tri_finder(*point1)
+        triangle2 = self.tri_finder(*point2)
+
+        min_tri_weight = min(self.triangle_weights[triangle1],
+                             self.triangle_weights[triangle2])
+
+        path_length = sqrt((s[0] - e[0]) ** 2 + (s[1] - e[1]) ** 2)
+        return path_length * min_tri_weight
+
+    def _base_triangulation_coordinates(self):
+        return [(x, y) for x, y in zip(self.triangulation.x, self.triangulation.y)]
+
+    def _create_graph(self, triangulation):
+        graph = []
+
+        coordinates = [(x, y) for x, y in zip(triangulation.x, triangulation.y)]
+
+        for edge in triangulation.edges:
+            graph.append((edge[0], edge[1], self.get_path_cost(coordinates[edge[0]],
+                                                               coordinates[edge[1]])))
+        return graph
+
+    def shortest_path(self, start=None, end=None):
+
+        vertices = self._base_triangulation_coordinates()
+
+        if start not in vertices:
+            vertices.append(start)
+
+        if end not in vertices:
+            vertices.append(end)
+
+        triangulation = tri.Triangulation([v[0] for v in vertices],
+                                          [v[1] for v in vertices])
+
+        coordinates = [(x, y) for x, y in zip(triangulation.x, triangulation.y)]
+
+        v1_index = coordinates.index(start)
+        v2_index = coordinates.index(end)
+
+        cost, path = dijkstra(self._create_graph(triangulation),
+                              v1_index,
+                              v2_index)
+
+        return cost, tuple(coordinates[idx] for idx in path)
--- esp/tests/test_esp.py
+++ esp/tests/test_esp.py
@@ -1,32 +1,73 @@
 from math import sqrt
 from unittest import TestCase
-import matplotlib.tri as tri
-import numpy as np
 
-from esp.dijkstra import dijkstra
-from esp.esp import triangulation_to_graph
+from esp.esp import ShortestPathFinder
+
+
+class TestGetPathCost(TestCase):
+    def test_minimumCostReturnedForPointsOnEdgeIrrespectiveOfOrdering(self):
+        vertices = [(0, 0),
+                    (0, 1),
+                    (1, 1),
+                    (1, 0)]
+
+        triangles = [(0, 1, 3),
+                     (1, 2, 3)]
+
+        triangle_weights = [1, 2]
+
+        start = (0, 1)
+        end = (1, 0)
+
+        pathFinder = ShortestPathFinder(vertices, triangles, triangle_weights=triangle_weights)
+
+        self.assertAlmostEqual(sqrt(2), pathFinder.get_path_cost(start, end))
+
+        pathFinder = ShortestPathFinder(vertices, triangles, triangle_weights=triangle_weights[::-1])
+
+        self.assertAlmostEqual(sqrt(2), pathFinder.get_path_cost(start, end))
 
 
 class TestShortestPathInSquare(TestCase):
     def setUp(self):
-        self.vertices = np.asarray([[0, 0],
-                                    [1, 0],
-                                    [0, 1],
-                                    [1, 1],
-                                    [1 / 2, 1 / 2]], dtype=np.float64)
+        self.vertices = [(0, 0),
+                         (1, 0),
+                         (0, 1),
+                         (1, 1),
+                         (1 / 2, 1 / 2)]
 
-        self.edges = np.asarray([[0, 2, 4],
-                                 [0, 4, 1],
-                                 [1, 4, 3],
-                                 [4, 2, 3]],dtype=np.int)
+        self.triangles = [[0, 2, 4],
+                          [0, 4, 1],
+                          [1, 4, 3],
+                          [4, 2, 3]]
 
-        self.triangulation = tri.Triangulation(self.vertices[:, 0],
-                                               self.vertices[:, 1],
-                                               triangles=self.edges)
-
+        self.pathFinder = ShortestPathFinder(self.vertices,
+                                             self.triangles)
 
     def test_shortestPathAlongEdges_isCornerToCorner(self):
-        graph = triangulation_to_graph(self.triangulation)
-        cost, path = dijkstra(graph,0,3)
-        self.assertAlmostEqual(sqrt(2),cost)
-        self.assertEqual((0,4,3),path)
+        cost, path = self.pathFinder.shortest_path(start=(0, 0),
+                                                   end=(1, 1))
+        self.assertAlmostEqual(sqrt(2), cost)
+        expected_path = ((0, 0),
+                         (1 / 2, 1 / 2),
+                         (1, 1))
+        self.assertEqual(expected_path, path)
+
+    def test_shortestPathAcrossSquare(self):
+        cost, path = self.pathFinder.shortest_path(start=(0, 1 / 2),
+                                                   end=(1, 1 / 2))
+        self.assertAlmostEqual(1, cost)
+        expected_path = ((0, 1 / 2),
+                         (1 / 2, 1 / 2),
+                         (1, 1 / 2))
+        self.assertEqual(expected_path, path)
+
+    def test_angledShortestPath(self):
+        cost, path = self.pathFinder.shortest_path(start=(0, 1 / 4),
+                                                   end=(1, 3 / 4))
+        expected_cost = sqrt(1 + 1 / 4)
+        expected_path = ((0, 1 / 4),
+                         (1 / 2, 1 / 2),
+                         (1, 3 / 4))
+        self.assertAlmostEqual(expected_cost, cost)
+        self.assertEqual(expected_path, path)
