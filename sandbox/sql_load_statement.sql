95e765a54d549cdbdbe2851a5d38f62a10c18462
mjms3 <shepherd.max@gmail.com>
2017-02-04 17:15:47 +0000

    Fix bug in sql load table statement

--- sandbox/sql_load_statement.sql
+++ sandbox/sql_load_statement.sql
@@ -3,7 +3,7 @@ into table esp.position_data
 fields terminated by ','
 optionally enclosed by '"' escaped by ''
 lines terminated by '\n' starting by ''
-(position_data_id, Id, Icao, Reg, Alt, GAlt,`Call`, CallSus, Lat, `Long`, Spd, Trak, `Type`, Mdl, Man, CNum, `From`, `To`, Op, OpCode, Mil, Cou, Gnd, @var1, operator_id)
+(Id, Icao, Reg, Alt, GAlt,`Call`, CallSus, Lat, `Long`, Spd, Trak, `Type`, Mdl, Man, CNum, `From`, `To`, Op, OpCode, Mil, Cou, Gnd, @var1)
 SET
 position_data_id = NULL,
 `TimeStamp` = STR_TO_DATE(@var1,'%Y-%m-%d-%H%iZ'),
