5cd922720f3d6779b8e4b5a6b22b29587e16e743
mjms3 <shepherd.max@gmail.com>
2017-03-19 19:56:16 +0000

    Update plotting to take into account new database schema.

--- sandbox/cost_calc.py
+++
@@ -1,8 +0,0 @@
-# cost of 1 implies 1 radian covered in unit time
-# 1 radian = radius of the earth = 6371 km
-# london to glasgow as the crow flies is 556 km
-# consider calculating this using the given data (change triangle weights to 1)
-# (heloesp) ➜  sandbox git:(master) ✗ python gb_map.py -0.05 0.96 0.0 0.9
-# {"COST": 0.0783040165594597, "POINTS": [[-0.05, 0.96], [-0.03866666666666667, 0.946], [-0.025, 0.933], [-0.005, 0.907666
-# 6371*0.0783 = 498.8 km (reasonable given the points aren't exactly london and glasgow)
-# so time is 6371*cost/speed_of_helicopter
--- sandbox/plot_start_and_end_points.py
+++ sandbox/plot_start_and_end_points.py
@@ -1,22 +1,52 @@
+from textwrap import dedent
+
 import matplotlib
-matplotlib.use('TkAgg')
-from mpl_toolkits.basemap import Basemap
+from matplotlib.colors import LogNorm
 
 from data_utilities.data_access_layer import DataAccessLayer
+
+matplotlib.use('TkAgg')
+
 import matplotlib.pyplot as plt
 
 dal = DataAccessLayer()
-Routes = dal.tbls.routes
+PositionReading = dal.tbls['position_reading']
+Route = dal.tbls['route']
 
-route_list = dal.session.query(Routes).all()
+route_points = list(dal.engine.execute(dedent("""\
+select latitude, longitude from
+position_reading
+where longitude between -6 and 2
+and latitude between 50 and 59
+""")))
+all_coords = [(float(p.latitude), float(p.longitude)) for p in route_points]
 
-m = Basemap(projection='merc',
-        resolution='l',llcrnrlat=50,llcrnrlon=-6,
-        urcrnrlat=59,urcrnrlon=2)
-m.drawcoastlines()
-take_off_coords =[m(float(r.route_start_long), float(r.route_start_lat)) for r in route_list]
-landing_coords =[m(float(r.route_end_long), float(r.route_end_lat)) for r in route_list]
-m.scatter([c[0] for c in landing_coords], [c[1] for c in landing_coords],5,'b')
-m.scatter([c[0] for c in take_off_coords], [c[1] for c in take_off_coords],5,'r')
-plt.show()
-input('')
+n_frames = 200
+n_skip = len(all_coords) // n_frames
+
+MIN_X = 575
+MAX_X = 1250
+def convert_x(x):
+    return MIN_X + (x+6)/8*(MAX_X-MIN_X)
+
+
+MIN_Y = 0
+MAX_Y = 1280
+def convert_y(y):
+    return MAX_Y - (y-50)/9*(MAX_Y-MIN_Y)
+
+img_data = plt.imread('background.jpg')
+
+
+plt.hist2d([convert_x(p[1]) for p in all_coords],[convert_y(p[0]) for p in all_coords],bins=50,alpha=0.5,norm=LogNorm(), zorder=2)
+plt.imshow(img_data,zorder=1)
+plt.xlim([MIN_X, MAX_X])
+plt.axis('off')
+plt.savefig('heatmap.png',dpi=300,bbox_inches='tight')
+
+
+# for i_frame in range(n_frames):
+    # coords = all_coords[i_frame * n_skip:n_skip * (i_frame + 1)]
+    # plt.scatter([convert_x(p[1]) for p in coords],[convert_y(p[0]) for p in coords],alpha=0.3, s=0.05,c='r')
+    #
+    # plt.savefig('frame{}.png'.format(i_frame), dpi=300,bbox_inches='tight')
